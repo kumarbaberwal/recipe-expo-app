e07a2ef27ead067ee1ca71c79eccc350bfe53406
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-06-26 02:01:02 +0530

    safescreen added

--- components/SafeScreen.tsx
+++ components/SafeScreen.tsx
@@ -1,9 +1,9 @@
 import { colors } from '@/constants/colors';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export default function SafeScreen({ children }: { children: React.ReactNode }) {
+export default function SafeScreen({ children }: PropsWithChildren) {
   const inset = useSafeAreaInsets();
   return (
     <View style={{ paddingTop: inset.top, flex: 1, backgroundColor: colors.background }}>
